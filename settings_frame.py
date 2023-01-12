3bc04755a967d827b643b8b9563af46aff6629e3
Roman Saraev <saraevrom@gmail.com>
2023-01-12 15:48:52 +0300

    Various display fixes.

--- settings_frame.py
+++ settings_frame.py
@@ -1,34 +1,34 @@
 import tkinter as tk
 from tkinter import ttk
+from tk_forms import ScrollView as ScrollableFrame
+
 
 # based on https://gist.github.com/mp035/9f2027c3ef9172264532fcd6262f3b01
-class ScrollableFrame(ttk.Frame):
-    def __init__(self, master, *args, **kwargs):
-        super(ScrollableFrame, self).__init__(master, *args, **kwargs)
-        self.view_canvas = tk.Canvas(self, borderwidth=0)
-        self.view_port = ttk.Frame(self.view_canvas)
-        self.view_canvas_window = self.view_canvas.create_window((0, 0), window=self.view_port, anchor="nw",
-                                                       tags="self.view_port")
-
-        self.vertical_scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.view_canvas.yview)
-        self.horizontal_scrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=self.view_canvas.xview)
-        self.view_canvas.configure(yscrollcommand=self.vertical_scrollbar.set,
-                                   xscrollcommand=self.horizontal_scrollbar.set)
-
-        self.view_canvas.grid(row=0, column=0, sticky="nsew")
-        self.horizontal_scrollbar.grid(row=1, column=0, sticky="nsew")
-        self.vertical_scrollbar.grid(row=0, column=1, sticky="nsew")
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-        # bind an event whenever the size of the viewPort frame changes.
-        self.view_port.bind("<Configure>", self.reconfigure_frame)
-        # bind an event whenever the size of the canvas frame
-        self.reconfigure_frame(None)
-
-    def reconfigure_frame(self, event):
-        self.view_canvas.configure(scrollregion=self.view_canvas.bbox("all"))
-
-
+# class ScrollableFrame(ttk.Frame):
+#     def __init__(self, master, *args, **kwargs):
+#         super(ScrollableFrame, self).__init__(master, *args, **kwargs)
+#         self.view_canvas = tk.Canvas(self, borderwidth=0)
+#         self.view_port = ttk.Frame(self.view_canvas)
+#         self.view_canvas_window = self.view_canvas.create_window((0, 0), window=self.view_port, anchor="nw",
+#                                                        tags="self.view_port")
+#
+#         self.vertical_scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.view_canvas.yview)
+#         self.horizontal_scrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=self.view_canvas.xview)
+#         self.view_canvas.configure(yscrollcommand=self.vertical_scrollbar.set,
+#                                    xscrollcommand=self.horizontal_scrollbar.set)
+#
+#         self.view_canvas.grid(row=0, column=0, sticky="nsew")
+#         self.horizontal_scrollbar.grid(row=1, column=0, sticky="nsew")
+#         self.vertical_scrollbar.grid(row=0, column=1, sticky="nsew")
+#         self.rowconfigure(0, weight=1)
+#         self.columnconfigure(0, weight=1)
+#         # bind an event whenever the size of the viewPort frame changes.
+#         self.view_port.bind("<Configure>", self.reconfigure_frame)
+#         # bind an event whenever the size of the canvas frame
+#         self.reconfigure_frame(None)
+#
+#     def reconfigure_frame(self, event):
+#         self.view_canvas.configure(scrollregion=self.view_canvas.bbox("all"))
 
 
 class SettingFormatError(Exception):
@@ -68,8 +68,8 @@ class Setting(tk.Frame):
     def get_value(self):
         raise NotImplementedError("Required to read value of setting")
 
-    def die(self,action):
-        raise SettingFormatError(self.setting_key,action)
+    def die(self, action):
+        raise SettingFormatError(self.setting_key, action)
 
     def reset(self):
         self.set_value(self.initial_value)
@@ -77,12 +77,9 @@ class Setting(tk.Frame):
     def set_dict_value(self, out_dict):
         out_dict[self.setting_key] = self.get_value()
 
-
     def get_dict_value(self, in_dict):
         if self.setting_key in in_dict.keys():
             self.set_value(in_dict[self.setting_key])
-
-
 
 
 class EntryValue(Setting):
@@ -309,7 +306,7 @@ class SettingMenu(ScrollableFrame):
         self.autocommiting = autocommit
         if not autocommit:
             self.commit_btn = ttk.Button(self, text="ОК", command=self.on_commit)
-            self.commit_btn.grid(row=2, column=0, sticky="ew")
+            self.commit_btn.grid(row=2, column=0, sticky="ew", columnspan=2)
         self.commit_action = None
         self.change_notify = dict()
         self.change_callbacks = dict()
@@ -340,17 +337,17 @@ class SettingMenu(ScrollableFrame):
             self.commit_action()
 
     def add_setting(self, setting_type, setting_key, display_name, initial_value, **kwargs):
-        newsetting = setting_type(self.view_port, setting_key, initial_value, **kwargs)
+        newsetting = setting_type(self.contents, setting_key, initial_value, **kwargs)
         newrow = self.get_new_row()
         self.user_settings.append(newsetting)
-        ttk.Label(self.view_port,text=display_name).grid(row=newrow, column=0, sticky="ew")
+        ttk.Label(self.contents,text=display_name).grid(row=newrow, column=0, sticky="ew")
         newsetting.grid(row=newrow, column=1, sticky="ew")
         if self.autocommiting:
             newsetting.add_on_edit_end_callback(self.autocommit_tracer)
 
     def add_separator(self,display_name):
         newrow = self.get_new_row()
-        ttk.Label(self.view_port, text=display_name, anchor="center", font='TkDefaultFont 10 bold').\
+        ttk.Label(self.contents, text=display_name, anchor="center", font='TkDefaultFont 10 bold').\
             grid(row=newrow, column=0, sticky="ew", columnspan=2)
         self.separator_count += 1
 
