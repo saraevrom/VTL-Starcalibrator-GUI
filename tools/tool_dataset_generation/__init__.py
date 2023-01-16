ada3765c8905d567409817ac6525b036eb8c3c48
Roman Saraev <saraevrom@gmail.com>
2023-01-16 17:05:28 +0300

    Dataset generator interface set up.

--- tools/tool_dataset_generation/__init__.py
+++ tools/tool_dataset_generation/__init__.py
@@ -4,7 +4,10 @@ from .create_settings import build_menu
 import gc
 import numpy as np
 import numba as nb
+from ..tool_flatfielder import FlatFieldingModel
+from common_GUI import GridPlotter, SettingMenu
 
+import tkinter as tk
 
 @nb.njit()
 def putmask_max(arr1,arr2):
@@ -34,24 +37,57 @@ def decimate_max(src,window):
 class DatasetGenerator(ToolBase):
     def __init__(self, master):
         super().__init__(master)
-        #self.setting_frame = SettingMenu(self)
-        self.columnconfigure(0, weight=1)
-        #self.rowconfigure(0, weight=1)
-        #self.rowconfigure(1, weight=1)
-        #self.setting_frame.grid(row=0, column=1, sticky="nsew")
-        #build_menu(self.setting_frame)
 
-        self.interval_editor = BgPickingEditor(self)
-        self.interval_editor.pack(fill="both", expand=True)
+        left_panel = tk.Frame(self)
 
+        left_panel.pack(side="left", fill="both", expand=True)
+
+        self.plotter = GridPlotter(left_panel)
+        self.plotter.pack(side="top", fill="both", expand=True)
+
+        self.interval_editor = BgPickingEditor(left_panel)
+        self.interval_editor.pack(side="bottom", fill="x")
+
+        right_panel = tk.Frame(self)
+        right_panel.pack(side="right", fill="y")
+
+        self.settings_menu = SettingMenu(right_panel, autocommit=True)
+        self.settings_menu.pack(fill="both", expand=True)
+        build_menu(self.settings_menu)
+        self.cutoff_1_setting = self.settings_menu.lookup_setting("cutoff_start")
+        self.cutoff_2_setting = self.settings_menu.lookup_setting("cutoff_end")
+        self.settings_dict = dict()
+        self.settings_menu.commit_action = self.on_settings_commit
+        self.settings_menu.add_tracer("filter_window", self.on_ma_filter_change)
         #self.plotter.grid(row=0, column=0, sticky="nsew", rowspan=2)
 
     def on_loaded_file_success(self):
         gc.collect()
-        # self.interval_editor.clear()
-        #
-        # model = None
-        # if os.path.isfile("flat_fielding.json"):
-        #     model = FlatFieldingModel.load("flat_fielding.json")
-        # self.interval_editor.display_data(self.file, model)
-        # self.interval_editor.draw()
+        self.settings_menu.push_settings_dict(self.settings_dict)
+        self.reload_data()
+        self.on_settings_commit()
+        gc.collect()
+
+    def reload_data(self):
+        model = self.get_ff_model()
+        self.interval_editor.reload_data(self.file, self.settings_dict, model)
+        self.propagate_limits(self.file["data0"].shape[0])
+
+    def on_ma_filter_change(self):
+        print("MA filter changed")
+        self.reload_data()
+
+    def on_settings_commit(self):
+        if self.file:
+            self.settings_menu.push_settings_dict(self.settings_dict)
+
+            print("Commit REDRAW")
+            self.interval_editor.redraw(self.settings_dict)
+
+    def propagate_limits(self, maxlen):
+        self.cutoff_1_setting.set_limits(0, maxlen)
+        self.cutoff_2_setting.set_limits(0, maxlen)
+
+    def on_ff_reload(self):
+        if self.file:
+            self.on_loaded_file_success()
--- tools/tool_dataset_generation/bg_picking_editor.py
+++ tools/tool_dataset_generation/bg_picking_editor.py
@@ -3,34 +3,71 @@ from ..tool_track_markup.denoising import reduce_noise, antiflash, moving_averag
 import numpy as np
 
 
+
+
 class BgPickingEditor(Plotter):
     def __init__(self, master):
         super().__init__(master)
+        self.draw_cache = []
+        self.axes.figure.set_size_inches(10, 2)
+        self.axes.get_xaxis().set_visible(False)
+        self.axes.get_yaxis().set_visible(False)
+        self.draw_y = None
+        self.figure.tight_layout()
+        self.toolbar.pack_forget()
+        self.mpl_canvas.get_tk_widget().configure(height=200)
+        self.pointer = self.axes.vlines(0, -1, 1)
+        self.pointer_location = 0
+
+    def on_left_click(self, event):
+        pass
+
+    def set_pointer(self, new_x):
+        self.pointer.set_xdata([new_x, new_x])
+        self.pointer_location = new_x
 
     def clear(self):
-        for line in self.axes.lines:
-            line.remove()
+        for x in self.draw_cache:
+            x.remove()
+        self.draw_cache.clear()
+        self.axes.set_prop_cycle(None)
 
-    def display_data(self,file, ff_model=None):
-        ys = np.array(file["data0"])
-        ys = ff_model.apply(ys)
+    def draw_data(self, settings):
+        cutoff_start = settings["cutoff_start"]
+        cutoff_end = settings["cutoff_end"]
+        if cutoff_end is None:
+            end = self.draw_y.shape[0]
+        else:
+            end = self.draw_y.shape[0]-cutoff_end
+        xs = np.arange(cutoff_start, end)
+        ys = self.draw_y[cutoff_start:end]
+        ys = ys/np.max(ys)
+        fill = self.axes.fill_between(xs, -ys, ys)
+        self.axes.set_xlim(cutoff_start, end-1)
+        self.draw_cache.append(fill)
+
+
+    def redraw(self, settings):
+        self.clear()
+        self.draw_data(settings)
+        self.draw()
+
+    def reload_data(self, file, settings, ff_model=None):
+        ys = file["data0"]
+        if ff_model:
+            ys = ff_model.apply(ys)
+        else:
+            ys = np.array(ys)
         print(ys.shape)
-        ys = moving_average_subtract(ys, 10)
+        ys = moving_average_subtract(ys, settings["filter_window"])
 
-        ys = reduce_noise(ys, 10)
-        ys = antiflash(ys)
 
         if ff_model:
             ys[:, ff_model.get_broken()] = 0
 
-        xs = np.arange(ys.shape[0])
-        #self.axes.plot(xs, np.max(ys, axis=(1, 2)))
-        # for i in range(HALF_PIXELS*2):
-        #     for j in range(HALF_PIXELS * 2):
-        maxes = np.max(ys, axis=0)
-        maxes_argsorted = np.dstack(np.unravel_index(np.argsort(maxes.ravel()), maxes.shape))[0]
-        maxed_indices = maxes_argsorted[-10:]
-        print(maxed_indices.shape)
-        for ij in maxed_indices:
-                i,j = ij
-                self.axes.plot(xs, ys[:, i, j])
+
+        draw_y = np.max(ys,axis=(1,2))
+        draw_y = draw_y/np.max(draw_y)
+        self.draw_y = draw_y
+        #draw_y = np.log(draw_y)
+
